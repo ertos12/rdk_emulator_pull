0f7dec1d8fa6ca646d0be3c8c3c8c1ad91fa2311
Matt Snoby <snobym@cisco.com>
2015-07-09 08:57:05 -0400

    corrected the name of the repository to clone.

--- rdk_emulator_pull.sh
+++ rdk_emulator_pull.sh
@@ -11,7 +11,7 @@ repo sync -c -j 16
 #
 # now pull the meta-cisco-oss from github
 #
-git clone https://github.com/snoby/rdk_emulator_pull.git
+git clone https://github.com/cisco/meta-cisco-oss.git
 
 #
 # we now have to tell the RDK yocto environment about our layer
